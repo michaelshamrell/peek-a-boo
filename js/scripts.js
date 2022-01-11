c0c465d812c4c36362bf2ceee2135eebb7340024
michaelshamrell <michaelshamrell2001@gmail.com>
2022-01-11 14:13:51 -0800

    wip, error with fadein, slide, etc..
    Jack Jones <jacktjones4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function() { 
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").fadeOut(); 
   });
 });
+
